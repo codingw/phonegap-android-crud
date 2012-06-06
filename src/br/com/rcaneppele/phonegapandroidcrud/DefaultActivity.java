d4327f567f80eac47154472652e12618a7da48a4
Rodrigo Ferreira <r.caneppele@gmail.com>
2012-06-05 22:58:07 -0300

    adicionando phonegap e jquery-mobile ao projeto

--- src/br/com/rcaneppele/phonegapandroidcrud/DefaultActivity.java
+++ src/br/com/rcaneppele/phonegapandroidcrud/DefaultActivity.java
@@ -1,13 +1,15 @@
 package br.com.rcaneppele.phonegapandroidcrud;
 
-import android.app.Activity;
+import org.apache.cordova.DroidGap;
+
 import android.os.Bundle;
 
-public class DefaultActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
+public class DefaultActivity extends DroidGap {
+
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
     }
+	
 }
